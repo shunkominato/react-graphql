fd4f1ae7eaa976f209c94ce224d95a88549ec8e8
shunkominato <shun19871001@gmail.com>
2022-03-17 08:20:13 +0900

    query

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import { ApolloProvider, Query } from 'react-apollo';
 import client from './client'
-import { ME } from './graphql'
+import { ME, SERCH_REPO } from './graphql'
 
-
+const VARIABLES = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア"
+}
 function App() {
+  const {first, after, last, before, query} = VARIABLES
   return (
     <ApolloProvider client={client}>
       <div >
         a
       </div>
-      <Query query={ME}>
+      <Query query={SERCH_REPO} variables={{first, after, last, before, query}}>
         {
           ({ loading, error, data }) => {
             if (loading) return 'loading...'
             if (error) return `${error.message}`
-            return <div>{data.user.url}</div>
+            console.log(data)
+            return <div></div>
           }
         }
       </Query>
